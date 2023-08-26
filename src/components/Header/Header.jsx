3804d5e0da1b84e52f8ac3b0299a4f130e72c4ec
VaitVhat52 <87101859+VaitVhat52@users.noreply.github.com>
2023-08-25 20:19:04 -0400

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -122,7 +122,7 @@ const Header = () => {
       <NavbarMenu>
         {menuItems.map((item, index) => (
           <NavbarMenuItem key={`${item}-${index}`}>
-            <Link className="w-full" href={`/${item.toLowerCase()}`} size="lg">
+            <Link className="w-full" href={`/${item.trim().toLowerCase()}`} size="lg">
               {item}
             </Link>
           </NavbarMenuItem>
