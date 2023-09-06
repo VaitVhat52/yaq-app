8081c6e0705585ae0db95e9553b4da01833a3648
VaitVhat52 <87101859+VaitVhat52@users.noreply.github.com>
2023-09-05 21:19:12 -0400

    Update page.jsx

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -20,7 +20,13 @@ const blog = () => {
     session();
   }, []);
 
-  return <div>blog</div>;
+  return (
+    <div className="container mt-20 mx-[17rem]">
+      <span>
+        <h1 className="text-4xl">Latest Posts</h1>
+      </span>
+    </div>
+  );
 };
 
 export default blog;
