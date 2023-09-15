7372346d17f227f72b9b9c5a0d6cb807303170c7
VaitVhat52 <87101859+VaitVhat52@users.noreply.github.com>
2023-09-15 17:53:59 -0400

    Update Identity.jsx

--- src/app/profile/components/Identity.jsx
+++ src/app/profile/components/Identity.jsx
@@ -1,12 +1,16 @@
 "use client";
 
 import { supabase } from "@/client";
+import { Button, Input } from "@nextui-org/react";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
+import { FaEdit } from "react-icons/fa";
 
 const Identity = () => {
   const [profile, setProfile] = useState("");
   const [name, setName] = useState("");
+  const [nameInput, setNameInput] = useState(name);
+  const [editing, setEditing] = useState(false);
 
   async function session() {
     const { data, error } = await supabase.auth.getSession();
@@ -17,13 +21,15 @@ const Identity = () => {
 
     setProfile(data.session.user.user_metadata.avatar_url);
     setName(data.session.user.user_metadata.full_name);
-
-    console.log(data);
   }
 
   useEffect(() => {
     session();
   });
+
+  function handleEdit() {
+    setEditing(true);
+  }
 
   return (
     <div className="flex flex-col justify-center items-center">
@@ -34,10 +40,33 @@ const Identity = () => {
         alt="Profile Picture"
         className="rounded-full mb-5"
       />
-      <h1 className="text-4xl text-center leading-normal">
-        Welcome,
-        <br className="sm:hidden inline" /> {name}
-      </h1>
+      <span className="inline whitespace-nowrap">
+        <h1 className="text-4xl text-center leading-normal">
+          Welcome,
+          <br className="sm:hidden inline" />
+          {!editing ? (
+            <> {name}</>
+          ) : (
+            <>
+              <Input
+                placeholder={name}
+                isClearable
+                size="lg"
+                className="-mb-7 mt-7"
+              />
+              <Button className="mt-10" color="danger">
+                Cancel
+              </Button>
+            </>
+          )}
+          &nbsp;
+          {!editing && (
+            <button onClick={handleEdit} className="cursor-pointer">
+              <FaEdit className="inline align-middle" size={15} />
+            </button>
+          )}
+        </h1>
+      </span>
     </div>
   );
 };
